a01a825b43c5214a683e2d05328dffa3356805fc
Bader Alshaya <alshayabader@gmail.com>
2020-09-09 22:21:03 +0300

    Update etl.py

--- p3/etl.py
+++ p3/etl.py
@@ -3,21 +3,38 @@ import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
 
-# Extracts the data from S3
+"""Extracts the data from S3
+
+Keyword arguments:
+cur -- the db cursor
+conn -- the db connection
+
+"""
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-# Stages the data in Redshift
+"""Stages the data in Redshift
+
+Keyword arguments:
+cur -- the db cursor
+conn -- the db connection
+
+"""
 def insert_tables(cur, conn):
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
-# Connect to the database and transforms data into a set of dimensional tables
+"""Connect to the database and transforms data into a set of dimensional tables
+
+Keyword arguments:
+NONE
+
+"""
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
